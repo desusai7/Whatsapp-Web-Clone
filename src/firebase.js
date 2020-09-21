d8e1e9ca5d5db8ef5a4d5e21fa9805751ec1f77a
desusai <saivenkatdesu@gmail.com>
2020-09-21 11:43:31 +0530

    Initial Commit

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCvxnKc5XLg8OhxAGd9YIJSMWv2KJM51aQ",
+    authDomain: "whatsapp-clone-71008.firebaseapp.com",
+    databaseURL: "https://whatsapp-clone-71008.firebaseio.com",
+    projectId: "whatsapp-clone-71008",
+    storageBucket: "whatsapp-clone-71008.appspot.com",
+    messagingSenderId: "583874174787",
+    appId: "1:583874174787:web:2e3789e8e6875c976799f6",
+    measurementId: "G-TMB7YL3DY1"
+  };
